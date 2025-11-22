a028e6be9188730ab1c92eddce2d57c624930ce7
rachmandika29 <rahmandikaadiguna@gmail.com>
2025-11-22 11:59:33 +0700

    refactor(accounts): move accounts controller and service to accounts module
    The AccountsController and AccountsService were moved from the AppModule to the AccountsModule to better organize the codebase and follow NestJS module best practices.

--- src/accounts/accounts.module.ts
+++ src/accounts/accounts.module.ts
@@ -1,4 +1,10 @@
 import { Module } from '@nestjs/common';
+import { AccountsController } from './accounts.controller';
+import { AccountsService } from './accounts.service';
 
-@Module({})
+@Module({
+  controllers: [AccountsController],
+  providers: [AccountsService],
+  exports: [AccountsService],
+})
 export class AccountsModule {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { AccountsController } from './accounts/accounts.controller';
-import { AccountsService } from './accounts/accounts.service';
 import { AccountsModule } from './accounts/accounts.module';
 
 @Module({
   imports: [AccountsModule],
-  controllers: [AppController, AccountsController],
-  providers: [AppService, AccountsService],
+  controllers: [AppController],
+  providers: [AppService],
 })
 export class AppModule {}
